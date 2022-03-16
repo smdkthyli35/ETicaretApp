5a54bd628b33ec3327f7e3dc9914f3664413599a
zeljkosamed <smdkthyli@gmail.com>
2022-03-16 19:03:58 +0300

    Repository pattern interfaceleri oluşturuldu.

---
+++ Core/ETicaretApp.Application/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretApp.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table { get; }
+    }
+}
