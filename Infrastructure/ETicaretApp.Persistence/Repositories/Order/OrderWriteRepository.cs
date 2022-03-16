8d0cc3f48f29c78e715a1e7d2742ef6f478502e1
zeljkosamed <smdkthyli@gmail.com>
2022-03-16 20:28:29 +0300

    Concrete repositoryler oluşturuldu.

---
+++ Infrastructure/ETicaretApp.Persistence/Repositories/Order/OrderWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretApp.Application.Repositories;
+using ETicaretApp.Domain.Entities;
+using ETicaretApp.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Persistence.Repositories
+{
+    public class OrderWriteRepository : WriteRepository<Order>, IOrderWriteRepository
+    {
+        public OrderWriteRepository(ETicaretAppDbContext context) : base(context)
+        {
+        }
+    }
+}
