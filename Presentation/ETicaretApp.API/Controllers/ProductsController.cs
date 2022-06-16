01435ef7e9bf43ead7a95dbf3dedfb3a5ea9af85
zeljkosamed <smdkthyli@gmail.com>
2022-06-16 18:05:22 +0300

    Pagination added.

--- Presentation/ETicaretApp.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretApp.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretApp.Application.Repositories;
+using ETicaretApp.Application.RequestParameters;
 using ETicaretApp.Application.ViewModels.Products;
 using ETicaretApp.Domain.Entities;
 using Microsoft.AspNetCore.Http;
@@ -25,9 +26,25 @@ namespace ETicaretApp.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false));
+            var totalCount = _productReadRepository.GetAll(false).Count();
+
+            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).ToList();
+
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
         }
 
         [HttpGet("{id}")]
