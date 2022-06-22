3ee9c1dd40a0192f79c9f05a1732c23b93f8d5cd
zeljkosamed <smdkthyli@gmail.com>
2022-06-22 15:01:55 +0300

    GetAllProductQueryRequest and GetAllProductQueryHandler added.

---
+++ Core/ETicaretApp.Application/Features/Queries/GetAllProduct/GetAllProductQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretApp.Application.RequestParameters;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application.Features.Queries.GetAllProduct
+{
+    public class GetAllProductQueryRequest : IRequest<GetAllProductQueryResponse>
+    {
+        public Pagination Pagination { get; set; }
+    }
+}
