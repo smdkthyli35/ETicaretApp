f84689a0884e0458f3ecbdc5cffac0e1d17826e9
zeljkosamed <smdkthyli@gmail.com>
2022-06-22 15:04:43 +0300

    Application ServiceRegistration added.

---
+++ Core/ETicaretApp.Application/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(ServiceRegistration));
+        }
+    }
+}
