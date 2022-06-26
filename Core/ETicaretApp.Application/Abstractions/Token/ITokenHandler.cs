f353ca2ddbd0ef2f8d1f6f452d3fff8017090c71
zeljkosamed <smdkthyli@gmail.com>
2022-06-26 17:37:12 +0300

    ITokenHandler and TokenHandler added.

---
+++ Core/ETicaretApp.Application/Abstractions/Token/ITokenHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application.Abstractions.Token
+{
+    public interface ITokenHandler
+    {
+        DTOs.Token CreateAccessToken(int minute);
+    }
+}
