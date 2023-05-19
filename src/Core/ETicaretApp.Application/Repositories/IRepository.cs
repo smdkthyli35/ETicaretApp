08ef9f260441dd96ada17d9c4d23c86205131875
Samed Kütahyalı <smdkthyli@gmail.com>
2023-05-19 19:33:09 +0300

    IRepository, IReadRepository and IWriteRepository have been added.

---
+++ src/Core/ETicaretApp.Application/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretApp.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Table { get; }
+    }
+}
