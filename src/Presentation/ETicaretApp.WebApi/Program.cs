7e32774cfa9e68436a9a5819ea9d074e849492aa
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-02 20:03:58 +0300

    CORS policy has been added.

--- src/Presentation/ETicaretApp.WebApi/Program.cs
+++ src/Presentation/ETicaretApp.WebApi/Program.cs
@@ -2,23 +2,27 @@ using ETicaretApp.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddPersistenceServices(builder.Configuration);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+{
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+}));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
