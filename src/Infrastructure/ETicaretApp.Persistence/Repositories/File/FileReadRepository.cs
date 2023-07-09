3eb9d3fe5fa03a6a58189c712ca39ff8d3852a3a
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-09 23:11:04 +0300

    Repository classes from the derived interface repository have been added

---
+++ src/Infrastructure/ETicaretApp.Persistence/Repositories/File/FileReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretApp.Application.Repositories;
+using ETicaretApp.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Persistence.Repositories
+{
+    public class FileReadRepository : ReadRepository<Domain.Entities.File>, IFileReadRepository
+    {
+        public FileReadRepository(ETicaretAppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/ETicaretApp.Persistence/Repositories/File/FileWriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretApp.Application.Repositories;
+using ETicaretApp.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Persistence.Repositories
+{
+    public class FileWriteRepository : WriteRepository<Domain.Entities.File>, IFileWriteRepository
+    {
+        public FileWriteRepository(ETicaretAppDbContext context) : base(context)
+        {
+        }
+    }
+}
