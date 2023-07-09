8783c39aebc41f6f6e9a6f7910fb18582ec1ad33
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-09 23:04:12 +0300

    BaseEntity has been updated.

--- src/Core/ETicaretApp.Domain/Entities/Common/BaseEntity.cs
+++ src/Core/ETicaretApp.Domain/Entities/Common/BaseEntity.cs
@@ -10,6 +10,6 @@ namespace ETicaretApp.Domain.Entities.Common
     {
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public virtual DateTime UpdatedDate { get; set; }
     }
 }
