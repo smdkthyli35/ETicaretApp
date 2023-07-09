9ce6c6adb6b275161d2e0797560f53b198675273
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-09 23:11:26 +0300

    ServiceRegistration has been updated.

--- src/Infrastructure/ETicaretApp.Persistence/ServiceRegistration.cs
+++ src/Infrastructure/ETicaretApp.Persistence/ServiceRegistration.cs
@@ -27,6 +27,12 @@ namespace ETicaretApp.Persistence
             services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
             services.AddScoped<IProductReadRepository, ProductReadRepository>();
             services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IFileReadRepository, FileReadRepository>();
+            services.AddScoped<IFileWriteRepository, FileWriteRepository>();
+            services.AddScoped<IProductImageFileReadRepository, ProductImageFileReadRepository>();
+            services.AddScoped<IProductImageFileWriteRepository, ProductImageFileWriteRepository>();
+            services.AddScoped<IInvoiceFileReadRepository, InvoiceFileReadRepository>();
+            services.AddScoped<IInvoiceFileWriteRepository, InvoiceFileWriteRepository>();
         }
     }
 }
