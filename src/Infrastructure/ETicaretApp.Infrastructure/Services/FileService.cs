40a89260382c8f939676d00639519d69e65e5f44
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-09 23:06:04 +0300

    FileService bug has been fixed.

--- src/Infrastructure/ETicaretApp.Infrastructure/Services/FileService.cs
+++ src/Infrastructure/ETicaretApp.Infrastructure/Services/FileService.cs
@@ -33,7 +33,7 @@ namespace ETicaretApp.Infrastructure.Services
             {
                 string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
                 bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
-                datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
+                datas.Add((fileNewName, $"{path}\\{fileNewName}"));
                 results.Add(result);
             }
 
