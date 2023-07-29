139dae0ae208c5cf640c4fc61df2d4965e0a2751
Samed Kütahyalı <smdkthyli@gmail.com>
2023-07-29 16:52:31 +0300

    Google login process has been added.

---
+++ src/Core/ETicaretApp.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using ETicaretApp.Application.Abstractions.Token;
+using ETicaretApp.Application.Dtos;
+using Google.Apis.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace ETicaretApp.Application.Features.Commands.AppUser.GoogleLogin
+{
+    public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
+    {
+        private readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        private readonly ITokenHandler _tokenHandler;
+        private readonly IConfiguration _configuration;
+
+        public GoogleLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _configuration = configuration;
+        }
+
+        public async Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { _configuration["Login:GoogleLogin"] },
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+
+            var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
+            Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+            bool result = user != null;
+
+            if (user is null)
+            {
+                user = await _userManager.FindByEmailAsync(payload.Email);
+                if (user is null)
+                {
+                    user = new()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = payload.Email,
+                        UserName = payload.Email,
+                        NameSurname = payload.Name,
+                    };
+
+                    var identityResult = await _userManager.CreateAsync(user);
+                    result = identityResult.Succeeded;
+                }
+            }
+
+            if (result)
+                await _userManager.AddLoginAsync(user, info);
+            else
+                throw new Exception("Invalid external authentication.");
+
+            Token token = _tokenHandler.CreateAccessToken(15);
+
+            return new()
+            {
+                Token = token
+            };
+        }
+    }
+}
---
+++ src/Core/ETicaretApp.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandResponse.cs
@@ -0,0 +1,9 @@
+﻿using ETicaretApp.Application.Dtos;
+
+namespace ETicaretApp.Application.Features.Commands.AppUser.GoogleLogin
+{
+    public class GoogleLoginCommandResponse
+    {
+        public Token Token { get; set; }
+    }
+}
