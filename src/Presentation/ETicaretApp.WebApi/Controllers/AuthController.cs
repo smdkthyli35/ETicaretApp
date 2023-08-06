173fc82ba7ce9a9df764bbaa914f8c1f70ec4487
Samed Kütahyalı <smdkthyli@gmail.com>
2023-08-06 13:29:07 +0300

    AuthController has been added

---
+++ src/Presentation/ETicaretApp.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using ETicaretApp.Application.Features.Commands.AppUser.FacebookLogin;
+using ETicaretApp.Application.Features.Commands.AppUser.GoogleLogin;
+using ETicaretApp.Application.Features.Commands.AppUser.LoginUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> GoogleLogin(GoogleLoginCommandRequest googleLoginCommandRequest)
+        {
+            GoogleLoginCommandResponse response = await _mediator.Send(googleLoginCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPost("facebook-login")]
+        public async Task<IActionResult> FacebookLogin(FacebookLoginCommandRequest facebookLoginCommandRequest)
+        {
+            FacebookLoginCommandResponse response = await _mediator.Send(facebookLoginCommandRequest);
+            return Ok(response);
+        }
+    }
+}
