1948e55b8a75f135e099afbd55c050d8f28de046
Samed Kütahyalı <smdkthyli@gmail.com>
2023-09-09 22:57:19 +0300

    BasketsController has been added.

---
+++ src/Presentation/ETicaretApp.WebApi/Controllers/BasketsController.cs
@@ -0,0 +1,52 @@
+﻿using ETicaretApp.Application.Features.Commands.Basket.AddItemToBasket;
+using ETicaretApp.Application.Features.Commands.Basket.RemoveBasketItem;
+using ETicaretApp.Application.Features.Commands.Basket.UpdateQuantity;
+using ETicaretApp.Application.Features.Queries.Basket.GetBasketItem;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BasketsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasketItems([FromQuery] GetBasketItemQueryRequest getBasketItemQueryRequest)
+        {
+            List<GetBasketItemQueryResponse> response = await _mediator.Send(getBasketItemQueryRequest);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddItemToBasket(AddItemToBasketCommandRequest addItemToBasketCommandRequest)
+        {
+            AddItemToBasketCommandResponse response = await _mediator.Send(addItemToBasketCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateQuantity(UpdateQuantityCommandRequest updateQuantityCommandRequest)
+        {
+            UpdateQuantityCommandResponse response = await _mediator.Send(updateQuantityCommandRequest);
+            return Ok(response);
+        }
+
+        [HttpDelete("BasketItemId")]
+        public async Task<IActionResult> RemoveBasketItem([FromRoute] RemoveBasketItemCommandRequest removeBasketItemCommandRequest)
+        {
+            RemoveBasketItemCommandResponse response = await _mediator.Send(removeBasketItemCommandRequest);
+            return Ok(response);
+        }
+    }
+}
