b0311eb6ff90cd0a01b995c373bc01a36d0d18b4
Samed Kütahyalı <smdkthyli@gmail.com>
2023-09-09 14:40:37 +0300

    Entities and database have been updated.

---
+++ src/Core/ETicaretApp.Domain/Entities/Basket.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretApp.Domain.Entities.Common;
+using ETicaretApp.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Domain.Entities
+{
+    public class Basket : BaseEntity
+    {
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+
+        public ICollection<BasketItem> BasketItems { get; set; }
+    }
+}
---
+++ src/Core/ETicaretApp.Domain/Entities/BasketItem.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretApp.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Domain.Entities
+{
+    public class BasketItem : BaseEntity
+    {
+        public Guid BasketId { get; set; }
+        public Basket Basket { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
